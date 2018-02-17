cfa9807ec02b9473b04f03dd430da57756868f0b
Dan Meisler <danmoshem@gmail.com>
2018-02-17 11:37:59 -0800

    Done with logic, remains only refactoring

--- Main.c
+++ Main.c
@@ -1,151 +1,231 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <ctype.h>
 #include "Stack.h"
 
-static Element parseNextElement()
-{
-	Element element;
-	unsigned char currentChar;
-	while( ( currentChar = getchar() ) == ' ' );
-	int isNumber = isdigit( currentChar );
-	ungetc( currentChar, stdin );
+#define MAX_STRING_EQUATION_SIZE 1000
 
-	if( isNumber )
+
+static Element *getElementsFromString( char *string )
+{
+	Element *elements = NULL, *newElements = NULL;
+	uint32_t elementsCount = 0;
+	uint32_t stringIndex = 0;
+	uint32_t currentBytesRead = 0;
+
+	while( string[stringIndex] != EOF )
 	{
-		element.kind = NUMERIC_ELEMENT;
-		scanf( "%lf", &element.number );
-	}
-	else
-	{ 
-		currentChar = getchar();
-		if( currentChar == '\n' )
+		newElements = (Element *)calloc( sizeof( Element ), elementsCount + 1 );
+
+		if( !newElements )
+			return NULL;
+
+		memcpy( newElements, elements, sizeof( Element ) * elementsCount );
+
+		if( elements )
+		    free( elements );
+
+		elements = newElements;
+
+		while( string[stringIndex] == ' ' )
+			++stringIndex;
+
+		if( string[stringIndex] == '\n' )
 		{
-			element.kind = LAST_ELEMENT;
+			elements[elementsCount].kind = LAST_ELEMENT;
+			break;
+		}
+		else if( isdigit( string[stringIndex] ) )
+		{
+			elements[elementsCount].kind = NUMERIC_ELEMENT;
+			sscanf( string + stringIndex, "%lf%n", &( elements[elementsCount].number ), &currentBytesRead );
 		}
 		else
-		{
-			element.kind = CHARACTER_ELEMENT;
-			element.character = currentChar;
+		{ 
+			elements[elementsCount].kind = CHARACTER_ELEMENT;
+			elements[elementsCount].character = string[stringIndex];
+			currentBytesRead = 1;
 		}
+
+		++elementsCount;
+		stringIndex += currentBytesRead;
 	}
 
-	return element;
+	return elements;
 }
 
-
-int main( void )
+static char matchingParenthesis( char closing )
 {
-	Stack stack;
-	Element topElement;
-	Element currentElement;
-	Element newElement;
-	int needParseNextElement = 1;
-	char currentChar = 0;
-	double currentNumber;
-	init( &stack );
+	switch( closing )
+	{
+		case ')':
+			return '(';
+		case ']':
+			return '[';
+		case '}':
+			return '{';
+	}
+	return '\0';
+}
 
-	printf( "Insert an equation:\n" );
+static int sumUp( Stack *stack, char closingParenthesis, double *result )
+{
+	Element topElement;
+	double sum = 0;
+
 	while( 1 )
 	{
-		if( needParseNextElement )
-			currentElement = parseNextElement();
-		else
-			needParseNextElement = 1;
+		topElement = pop( stack );
 
-		if( currentElement.kind == LAST_ELEMENT )
+		if ( topElement.kind == NUMERIC_ELEMENT )
+		{	
+			sum += topElement.number;
+			continue;
+		}
+
+		switch( closingParenthesis )
 		{
-			newElement.kind = NUMERIC_ELEMENT;
-			newElement.number = 0;
-			while( 1 )
-			{
-				topElement = pop( &stack );
+			case '\0':
 				if( topElement.kind == END_OF_STACK_ELEMENT )
 				{
-					push( &stack, newElement );
-					break;
+					*result = sum;
+					return 1;
 				}
-                else
+				break;
+
+			case ')':
+			case ']':
+			case '}':
+				if( topElement.kind == CHARACTER_ELEMENT && topElement.character == matchingParenthesis( closingParenthesis ) ) 
 				{
-					if( topElement.kind != NUMERIC_ELEMENT )
-						return 1;
-					newElement.number += topElement.number;
+					*result = sum;
+					return 1;
 				}
-			}
-			break;
-				
+				break;
 		}
-		else if( currentElement.kind == NUMERIC_ELEMENT )
+
+		return 0;
+	}
+
+	return 0; 
+}
+
+static int evaluate( char *stringEquation, double *result )
+{
+	Stack stack;
+	Element *elements;
+	Element topElement,currentElement, newElement;
+	uint32_t currentElementIndex = 0;
+
+	init( &stack );
+
+	elements = getElementsFromString( stringEquation );
+
+	currentElement = elements[0];
+
+	while( currentElement.kind != LAST_ELEMENT )
+	{
+		if( currentElement.kind == NUMERIC_ELEMENT )
 		{
 			topElement = top( &stack );
-			if( topElement.kind == END_OF_STACK_ELEMENT )
+            
+			switch( topElement.kind )
 			{
-				push( &stack, currentElement );
-			}
-			else if( topElement.kind == CHARACTER_ELEMENT && topElement.character == '(' )
-			{
-				push( &stack, currentElement );
-			}
-			else if( topElement.kind == CHARACTER_ELEMENT && topElement.character == '+' )
-			{
-				pop( &stack );
-				push( &stack, currentElement );
-			}
-	        else if( topElement.kind == CHARACTER_ELEMENT && topElement.character == '-' )
-			{
-				pop( &stack );
-				currentElement.number *= -1;
-				push( &stack, currentElement );
-			}
-			else if( topElement.kind == CHARACTER_ELEMENT && topElement.character == '*' )
-			{
-				pop( &stack );
-				topElement = pop( &stack );
-				newElement.kind = NUMERIC_ELEMENT;
-				newElement.number = topElement.number * currentElement.number;
-				push( &stack, newElement );
-			}
-            else if( topElement.kind == CHARACTER_ELEMENT && topElement.character == '/' )
-			{
-				pop( &stack );
-				topElement = pop( &stack );
-				newElement.kind = NUMERIC_ELEMENT;
-				newElement.number = topElement.number / currentElement.number;
-				push( &stack, newElement );
-			}
+				case END_OF_STACK_ELEMENT:
+					push( &stack, currentElement );
+					break;
+				
+				case NUMERIC_ELEMENT:
+					return 0;
 
+				case CHARACTER_ELEMENT:
+					switch( topElement.character )
+					{
+						case '(':
+						case '[':
+						case '{':
+				            push( &stack, currentElement );
+							break;
+						
+						case '+':
+                        	pop( &stack );
+							push( &stack, currentElement );
+							break;
+
+						case '-':
+                        	pop( &stack );
+							currentElement.number *= -1;
+							push( &stack, currentElement );
+							break;
+
+						case '*':
+							pop( &stack );
+				 			topElement = pop( &stack );
+							newElement.kind = NUMERIC_ELEMENT;
+							newElement.number = topElement.number * currentElement.number;
+							push( &stack, newElement );
+							break;
+
+						case '/':
+							pop( &stack );
+							topElement = pop( &stack );
+							newElement.kind = NUMERIC_ELEMENT;
+							newElement.number = topElement.number / currentElement.number;
+							push( &stack, newElement );
+							break;
+
+					}
+			}
 		}
 		else if( currentElement.kind == CHARACTER_ELEMENT )
 		{
 			switch( currentElement.character )
 			{
 				case ')':
+				case ']':
+				case '}':
 					newElement.kind = NUMERIC_ELEMENT;
-					newElement.number = 0;
-					while( 1 )
-					{
-						 topElement = pop( &stack );
-						 if( topElement.kind == END_OF_STACK_ELEMENT )
-							 return 1;
-						 else if( topElement.kind == CHARACTER_ELEMENT && topElement.character == '(' )
-						 {
-                             currentElement = newElement;
-			 	             needParseNextElement = 0;
-							 break;
-						 }
-						 else
-						 {
-							 newElement.number += topElement.number;
-						 }
-					}
-					break;
+					if( !sumUp( &stack, currentElement.character, &( newElement.number ) ) )
+						return 0;
+					currentElement = newElement;
+					continue;
 				default:
 					push( &stack, currentElement );
 					break;
 			}
 		}
+
 		printStack( &stack );
+	    currentElement = elements[++currentElementIndex];
 	}
-    printStack( &stack );
+
+	free( elements );
+
+	if( !sumUp( &stack, '\0', result ) )
+		return 0;
+
 	destroy( &stack );
+
+	return 1;
+}
+
+int main( void )
+{
+	char stringEquation[MAX_STRING_EQUATION_SIZE] = { 0 };
+	double result;
+
+	printf( "Insert an equation:\n" );
+
+	fgets( stringEquation, MAX_STRING_EQUATION_SIZE, stdin );
+
+	if( !evaluate( stringEquation, &result ) )
+	{
+		printf( "Some error occured\n" );
+		return 1;
+	}
+
+	printf( "result = %lf\n", result );
+
 	return 0;
 }
